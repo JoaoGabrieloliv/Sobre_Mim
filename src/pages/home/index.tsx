5b2a2904553f7cb274c18eb6a85f34f8e0c2160b
JOAO GABRIEL OLIVEIRA SOUZA LIMA SALES <joao.sales@demprel.local>
2024-08-02 14:19:29 -0300

    Joao SobreMim Primeiro COmmit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,27 @@
+import { Social } from "../../components/social";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+export function Home() {
+  return (
+    <div className="flex flex-col w-full py-4 items-center justify-center">
+      <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">
+        João Gabriel Oliveira
+      </h1>
+      <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
+
+      <main className="flex flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+          <a>
+            <p className="text-base md:text-lg">GitHub</p>
+          </a>
+        </section>
+
+        <footer className="flex justify-center gap-3 my-4">
+          <Social url="https://github.com/JoaoGabrieloliv">
+            <FaGithub size={35} color="#FFF" />
+          </Social>
+        </footer>
+      </main>
+    </div>
+  );
+}
